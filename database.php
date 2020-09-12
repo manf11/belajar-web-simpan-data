4dd5cb0cf639b5c85ce1a1497cda4852507f1a4f
manf11 <mohahlisnoorfuad1@gmail.com>
2020-09-12 15:28:26 +0700

    create table product & product kategori

---
+++ database.php
@@ -0,0 +1,29 @@
+<?php
+$server="localhost";
+$user="root";
+$password="";
+$db_name="db_kopiku";
+
+$koneksi=mysqli_connect($server,$user,$password,$db_name);
+  if(!$koneksi){
+  die("koneksi gagal".mysqli_connect_error());
+}
+$sql="CREATE TABLE product_category(
+  id INT(128)UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nama_kopi VARCHAR(50) NOT NULL)";
+
+$sql="CREATE TABLE product(
+    id INT(128)UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    product_kategori_id VARCHAR(50) NOT NULL,
+    name VARCHAR(100),
+    price INT)";
+$sql="CREATE TABLE product_category(
+    id INT(128)UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nama_kopi VARCHAR(50) NOT NULL)";
+  if(mysqli_query($koneksi,$sql,$sql_product)){
+      echo "tabel berhasil di buat";
+    }else{
+      echo "tabel error" .mysqli_error($koneksi);
+    }
+  mysqli_close($koneksi);
+?>  
